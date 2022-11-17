599901ec1331792d61b79bab28c6334f316d94c2
Muriithi <muriithi.kibata@student.moringaschool.com>
2022-11-17 11:07:56 +0300

    Done

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solution here
+function findMatching (drivers, attribute) {
+ let filtered = drivers.filter((item)=> item.toLowerCase() === attribute.toLowerCase())
+ return filtered
+}
+
+console.log(findMatching(['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby', 'bobby'], "Bobby"))
+
+function fuzzyMatch (drivers, attribute) {
+let filtered = drivers.filter((item)=> item.startsWith(attribute))
+return filtered
+}
+
+function matchName (drivers, attribute) {
+  let filtered =  drivers.filter((item) => item.name === attribute)
+  return filtered
+}
